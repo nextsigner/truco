637b6013a25d2c57f1714375564ff1e8c6c9a121
nextsigner <nextsigner@gmail.com>
2024-04-14 23:46:43 -0300

    Se agregan imgs

--- main.qml
+++ main.qml
@@ -1,21 +1,172 @@
 import QtQuick 2.7
+import QtQuick.Window 2.0
 import QtQuick.Controls 2.0
 import Qt.labs.settings 1.1
+import ZoolLogView 1.0
 
 ApplicationWindow{
     id: app
     visible: true
     visibility: 'Maximized'
     title: 'Truco'
+    width: Screen.width
+    height: Screen.height
     color: apps.backgroundColor
+
+    property int fs: 60
+    property url host: 'http://localhost'
+    property int port: 8200
+    property url url: host+':'+port
     Settings{
         id: apps
         property color backgroundColor: 'black'
         property color fontColor: 'white'
+        property bool showLog: true
     }
     Item{
         id: xApp
         anchors.fill: parent
 
+        ZoolLogView{id: log}
+
+        Row{
+            anchors.right: parent.right
+            Button{
+                text: 'Repartir'
+                onClicked: repartir()
+            }
+        }
     }
+    Timer{
+        id: tGetStatus
+        running: false//true
+        repeat: true
+        interval: 5000
+        onTriggered: {
+            let d=new Date(Date.now())
+            let url=app.url
+
+            //Nuevo Evento
+            //url+='/truco/nuevoEvento'
+            //url+='?e=evento_'+d.getTime()
+            //url+='&nj=1'
+
+            //Get Eventos
+            url+='/truco/getEventos'
+
+
+
+            log.lv('url: '+url)
+            app.getRD(url, nuevoEvento)
+        }
+    }
+    //-->HTTP
+    QtObject{
+        id: nuevoEvento
+        function setData(data, isData){
+            //if(app.dev){
+                //log.lv('nuevoEvento:\n'+JSON.stringify(JSON.parse(data), null, 2))
+            //}
+            if(isData){
+                let dr
+                let j=JSON.parse(data)
+                if(j.isRec){
+                    if(j.evento.e.indexOf('repartir_')===0){
+                        let m0=j.evento.e.split('_')
+                        dr=new Date(parseInt(m0[1]))
+                        let h=dr.getHours()
+                        let min=dr.getMinutes()
+                        let sec=dr.getSeconds()
+                        log.lv('['+h+':'+min+':'+sec+']: Se repartieron cartas.')
+                        //log.lv('Evento: '+j.evento.e)
+                    }
+
+                    //if(app.dev){
+                        //log.lv('New remote params, id: '+j.params._id)
+                    //}
+                    //app.j.showMsgDialog('Zool Informa', 'Los datos se han guardado.', 'Una copia del archivo '+app.currentNom+' ha sido respaldado en el servidor de Zool.')
+                }else{
+                    //app.j.showMsgDialog('Zool Informa Error!', 'Los datos no han sido guardados.', j.msg)
+                }
+
+            }else{
+                //app.j.showMsgDialog('Zool Informa', 'Los datos no se han guardado en el servidor.', 'No se ha copia del archivo '+app.currentNom+'. No ha sido respaldado en el servidor de Zool.\nPosiblemente usted no esté conectado a internet o el servidor de Zool no se encuentra disponible en estos momentos.')
+            }
+        }
+    }
+    QtObject{
+        id: setEventos
+        function setData(data, isData){
+            //if(app.dev){
+                log.lv('setEventos:\n'+JSON.stringify(JSON.parse(data), null, 2))
+            //}
+            if(isData){
+                let j=JSON.parse(data)
+                if(j.isRec){
+                    //if(app.dev){
+                        log.lv('New remote params, id: '+j.params._id)
+                    //}
+                    //app.j.showMsgDialog('Zool Informa', 'Los datos se han guardado.', 'Una copia del archivo '+app.currentNom+' ha sido respaldado en el servidor de Zool.')
+                }else{
+                    //app.j.showMsgDialog('Zool Informa Error!', 'Los datos no han sido guardados.', j.msg)
+                }
+
+            }else{
+                //app.j.showMsgDialog('Zool Informa', 'Los datos no se han guardado en el servidor.', 'No se ha copia del archivo '+app.currentNom+'. No ha sido respaldado en el servidor de Zool.\nPosiblemente usted no esté conectado a internet o el servidor de Zool no se encuentra disponible en estos momentos.')
+            }
+        }
+    }
+
+    //<--HTTP
+    Shortcut{
+        sequence: 'Esc'
+        onActivated: Qt.quit()
+    }
+    Component.onCompleted: {
+        log.lv('Inicio.')
+    }
+    //-->Internet
+    function getRD(url, item){//Remote Data
+        var request = new XMLHttpRequest()
+        request.open('GET', url, true);
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status && request.status === 200) {
+                    /*let d=request.responseText
+                    if(d.indexOf('redirected')>=0&&d.indexOf('</html>')>=0){
+                        let m0=d.split('</html>')
+                         item.setData(m0[1], true)
+                    }else{
+                         item.setData(d, true)
+                    }*/
+                    item.setData(parseRetRed(request.responseText), true)
+                } else {
+                    item.setData("Url: "+url+" Status:"+request.status+" HTTP: "+request.statusText, false)
+                }
+            }
+        }
+        request.send()
+    }
+    function parseRetRed(d){
+        if(d.indexOf('redirected')>=0&&d.indexOf('</html>')>=0){
+            let m0=d.split('</html>')
+            return m0[1]
+        }else{
+            return d
+        }
+    }
+    //<--Internet
+
+    //-->Funciones
+    function repartir(){
+        let d=new Date(Date.now())
+        let url=app.url
+        url+='/truco/nuevoEvento'
+        url+='?e=repartir_'+d.getTime()
+        url+='&nj=0'
+        //log.lv('url: '+url)
+        app.getRD(url, nuevoEvento)
+    }
+    //<--Funciones
 }
+
--- server/eventos.js
+++ server/eventos.js
@@ -5,53 +5,148 @@
         e: String //Evento
         nj: Number //Numero de Jugador
     */
+
+    var aCartas=['1o', '2o', '3o', '4o', '5o', '6o', '7o', '10o', '11o', '12o', '1c', '2c', '3c', '4c', '5c', '6c', '7c', '10c', '11c', '12c', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '10b', '11b', '12b', '1e', '2e', '3e', '4e', '5e', '6e', '7e', '10e', '11e', '12e']
+
+    var pie='Nadie'
+    var uCJ1=[]
+    var uCJ2=[]
+
     nuevoEvento = function(req, res){
-        console.log('Insertando ZoolUser con nombre '+req.query.n)
+        console.log('Insertando Evento con nombre '+req.query.e)
         Evento.find({
-                          //date: {$gt: h }
-                          //date: {$gte: "2019-06-12T00:00:00+01:00", $lte: "2019-12-12T23:00:00+01:00" }
-                          //date: {$gte: h, $lte: hf }
-                          e:req.query.n // Search Filters
-                      },
-                      ['n'], // Columns to Return
-                      {
-                          skip:0, // Starting Row
-                          limit:1, // Ending Row
-                          sort:{
-                              n: 1 //Sort by Date Added DESC
-                          }
-                      },
-                      function(err, resultados){
-                          if(err) res.status(500).send({mensaje: `Error al buscar mensajes: ${err}`})
-                          if(resultados.length===0){
-                              console.log('Registrando el dato del ZoolUser '+req.query.nombre);
-                              //res.redirect('/res-add-producto.html?res=no'+mensajes.length)
+                        //date: {$gt: h }
+                        //date: {$gte: "2019-06-12T00:00:00+01:00", $lte: "2019-12-12T23:00:00+01:00" }
+                        //date: {$gte: h, $lte: hf }
+                        e:req.query.e // Search Filters
+                    },
+                    ['n'], // Columns to Return
+                    {
+                        skip:0, // Starting Row
+                        limit:1, // Ending Row
+                        sort:{
+                            n: 1 //Sort by Date Added DESC
+                        }
+                    },
+                    function(err, resultados){
+                        if(err) res.status(500).send({mensaje: `Error al buscar mensajes: ${err}`})
+                        if(resultados.length===0){
+                            console.log('Registrando el dato del Evento '+req.query.e);
+                            //res.redirect('/res-add-producto.html?res=no'+mensajes.length)
 
-                              var evento = new Evento()
-                              evento.e  = req.query.e
-                              evento.nj = req.query.nj
-                              console.log('Creando un nuevo evento: '+evento.e+'\nClave: '+zoolUser.c)
-                              evento.save(function(err, eventoRegistered){
-                                  if(err){
-                                      res.status(500).send(`Error when user register: ${err}`)
-                                      return
-                                  }
-                                  //res.redirect('/res-add-producto.html?res=El+producto+se+ha+agregado+correctamente&pid='+userRegistered._id)
+                            var evento = new Evento()
+                            evento.e  = req.query.e
+                            evento.nj = req.query.nj
+                            console.log('Creando un nuevo evento: '+evento.e+'\nJugador: '+evento.nj)
+                            evento.save(function(err, eventoRegistered){
+                                if(err){
+                                    res.status(500).send(`Error when user register: ${err}`)
+                                    return
+                                }
+                                //res.redirect('/res-add-producto.html?res=El+producto+se+ha+agregado+correctamente&pid='+userRegistered._id)
+                                let jsonRes={evento: eventoRegistered, isRec: true}
+                                if(req.query.e.indexOf('repartir_')===0){
+                                    const cartas = generarNumerosAleatorios();
+                                    let ac=[aCartas[cartas[0]], aCartas[cartas[1]], aCartas[cartas[2]], aCartas[cartas[3]], aCartas[cartas[4]], aCartas[cartas[5]]]
+                                    uCJ1=[ac[0], ac[1], ac[2]]
+                                    uCJ2=[ac[3], ac[4], ac[5]]
+                                    console.log('Cartas: '+cartas);
+                                    console.log('Cartas ac: '+ac);
+                                    jsonRes={evento: eventoRegistered, cartas: ac, isRec: true}
+                                    console.log('JSON CARTAS: '+JSON.stringify(jsonRes, null, 2));
+                                }
 
-                                  let jsonRes={evento: eventoRegistered, isRec: true}
-                                  console.log('jsonRes: '+JSON.stringify(jsonRes))
-                                  res.status(200).send(jsonRes)
-                              })
-                              return
-                          }else{
-                              console.log('Se intenta repetir el registro del evento '+req.query.e);
-                              //var msg='No+se+ha+registrado+el+productoYa+existe+un+producto+con+el+nombre+'+(''+req.query.n).replace(/ /g, '%20')
-                              //res.redirect('/res-add-producto.html?res='+msg)
-                              let jsonRes={evento:{}, isRec: false, msg: 'En la base de datos de Truco ya existe un usuario con el evento '+req.query.e+'.'}
-                              res.status(200).send(jsonRes)
-                          }
-                      })
+                                console.log('jsonRes: '+JSON.stringify(jsonRes))
+                                res.status(200).send(jsonRes)
+                            })
+                            return
+                        }else{
+                            console.log('Se intenta repetir el registro del evento '+req.query.e);
+                            //var msg='No+se+ha+registrado+el+productoYa+existe+un+producto+con+el+nombre+'+(''+req.query.n).replace(/ /g, '%20')
+                            //res.redirect('/res-add-producto.html?res='+msg)
+                            let jsonRes={evento:{}, isRec: false, msg: 'En la base de datos de Truco ya existe un usuario con el evento '+req.query.e+'.'}
+                            res.status(200).send(jsonRes)
+                        }
+                    })
     }
+
+    getEventos = function(req, res){
+        console.log('Listando eventos')
+        Evento.find({
+                        //date: {$gt: h }
+                        //date: {$gte: "2019-06-12T00:00:00+01:00", $lte: "2019-12-12T23:00:00+01:00" }
+                        //date: {$gte: h, $lte: hf }
+                        //e:req.query.n // Search Filters
+                    },
+                    [], // Columns to Return
+                    {
+                        skip:0, // Starting Row
+                        limit:1, // Ending Row
+                        sort:{
+                            fechaRegistro: 1 //Sort by Date Added DESC
+                        }
+                    },
+                    function(err, resultados){
+                        if(err) res.status(500).send({mensaje: `Error al buscar mensajes: ${err}`})
+                        if(resultados.length===0){
+                            console.log('No hay eventos ');
+                            let jsonRes={evento: resultados, isRec: true}
+                            console.log('jsonRes: '+JSON.stringify(jsonRes))
+                            res.status(200).send(jsonRes)
+                            return
+                        }else{
+                            console.log('Se muestran todos los eventos registrados');
+                            let jsonRes={evento: resultados, isRec: true}
+                            console.log('jsonRes: '+JSON.stringify(jsonRes))
+                            res.status(200).send(jsonRes)
+                        }
+                    })
+    }
+
+    getCartas = function(req, res){
+        console.log('Consultando cartas: '+req.query.nj)
+        if(req.query.nj==='1'){
+            let jsonRes={cartas: uCJ1, isRec: true}
+            res.status(200).send(jsonRes)
+            return
+        }
+        if(req.query.nj==='2'){
+            let jsonRes={cartas: uCJ2, isRec: true}
+            res.status(200).send(jsonRes)
+            return
+        }
+    }
+
+
+//    getEventos = function(res, req){
+//        Evento.findOne().sort({ _id: -1 }).exec((err, ultimoDocumento) => {
+//                                                    if (err) {
+//                                                        console.error('Error al obtener el último documento:', err);
+//                                                        return;
+//                                                    }
+
+//                                                    // El último documento está almacenado en la variable `ultimoDocumento`
+//                                                    let jsonRes={eventos: ultimoDocumento, isRec: true}
+//                                                    console.log('jsonRes: '+JSON.stringify(jsonRes))
+//                                                    //res.json(jsonRes);
+//                                                    res.status(200).send(jsonRes)
+//                                                    console.log('Último documento:', ultimoDocumento);
+//                                                });
+//    }
+
+    function generarNumerosAleatorios() {
+      const numeros = [];
+      while (numeros.length < 6) {
+        const numero = Math.floor(Math.random() * 40) + 1;
+        if (!numeros.includes(numero)) {
+          numeros.push(numero);
+        }
+      }
+      return numeros;
+    }
+
     app.get('/truco/nuevoEvento', nuevoEvento);
+    app.get('/truco/getEventos', getEventos);
+    app.get('/truco/getCartas', getCartas);
 }
 
